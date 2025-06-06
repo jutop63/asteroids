2bc614159b1c0d164fb3672491cbe377bebc097d
jutop63 <luketacy@gmail.com>
2025-06-06 13:40:47 -0400

    Update player main

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
     running = True 
     while running:
         for event in pygame.event.get():
@@ -16,10 +18,11 @@ def main():
                 return
             
 
-        screen.fill
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
-
-        pygame.display.flip
+        #limit the framerate to 60 fps
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
